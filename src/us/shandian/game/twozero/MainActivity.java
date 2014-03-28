ef76f46e037b6d29737f2789e0b0e067382ae2a4
PeterCxy <xqsx43cxy@126.com>
2014-03-28 19:15:44 +0800

    MainActivity: Hardware acceleration
    much smoother and clearer

--- src/us/shandian/game/twozero/MainActivity.java
+++ src/us/shandian/game/twozero/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
@@ -13,8 +14,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        /*getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
         view = new MainView(getBaseContext());
         if (savedInstanceState != null) {
             Tile[][] field = view.game.grid.field;
